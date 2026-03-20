f1c1222d46c9e236d27704936e44050fff8b5fff
Igor da Fonseca <igorfonseca-sc@hotmail.com>
2026-03-19 21:55:23 -0300

    Implementação parcial do conteudo da aula 2

--- avaliacao01-prw2/src/componentes/Formulario/Formulario.jsx
+++ avaliacao01-prw2/src/componentes/Formulario/Formulario.jsx
@@ -1,13 +1,31 @@
 import React from "react";
 import "./Formulario.css";
 import CampoTexto from "../CampoTexto/CampoTexto";
+import ListaSuspensa from "../ListaSuspensa/ListaSuspensa";
 const Formulario = () => {
+    const secoesLoja = [
+        'Computadores',
+        'Acessórios',
+        'Impressoras',
+        'Games',
+        'Gadjets'
+    ]
+    const marcas = [
+        'Huawei',
+        'HP',
+        'Del',
+        'Corsair',
+        'Stean'
+    ]
+
   return (
     <section className="formulario">
       <form>
         <h1>Dados do produto</h1>
         <CampoTexto label="Nome" placeholder="Digite o nome do produto" />
         <CampoTexto label="Preco" placeholder="Digite o preço do produto" />
+        <ListaSuspensa label="Seção da loja" itens={secoesLoja} />
+        <ListaSuspensa label="Marca" itens={marcas} />
       </form>
     </section>
   );
